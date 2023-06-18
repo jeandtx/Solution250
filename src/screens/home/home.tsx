808bee95c98948d89e841c1590ae098fae3b623c
jeandtx <jean.doutriaux@gmail.com>
2023-06-18 16:36:16 +0200

    added navbar & footer to homepage

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './home.module.scss';
 import { TextInput } from '../../components/text-input/text-input';
+import { Navbar } from '@blueprintjs/core';
+import { Footer } from '../../components/footer/footer';
 
 export const Home: React.FC = () => {
     return (
         <div>
+            <Navbar />
             <TextInput />
+            <Footer />
         </div>
     );
 };
