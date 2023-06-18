0ecc59b33f42dc76d683b045c74213d518bfdca0
jeandtx <jean.doutriaux@gmail.com>
2023-06-18 19:09:42 +0200

    IAPrediction working but model not fitting

--- src/components/ia-prediction/ia-prediction.tsx
+++ src/components/ia-prediction/ia-prediction.tsx
@@ -1,7 +1,7 @@
-import { Elevation, Card, InputGroup, H1 } from '@blueprintjs/core';
+import { Elevation, Card, InputGroup, H1, Button } from '@blueprintjs/core';
 import classNames from 'classnames';
 import styles from './ia-prediction.module.scss';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { loadLayersModel, tensor2d } from '@tensorflow/tfjs';
 
 export interface FormCardProps {
@@ -10,41 +10,67 @@ export interface FormCardProps {
 
 export const IAPrediction = ({ pathToModel }: FormCardProps) => {
     const [text, setText] = useState('');
+    const [output, setOutput] = useState('');
 
-    useEffect(() => {
-        const loadModel = async () => {
-            const model = await loadLayersModel(
-                'src/assets/polarity/model.json'
+    const loadModel = async () => {
+        const model = await loadLayersModel('src/assets/polarity/model.json');
+        return model;
+    };
+
+    const tokenize = async (word: string) => {
+        try {
+            const tokenizer: any = await import(
+                '../../assets/polarity/tokenizer.json'
             );
-            return model;
-        };
-
-        const predict = async (text: string, model: any) => {
-            const text_array = text.split(' ');
-            const padded_text_array = text_array
-                .slice(0, 500)
-                .concat(Array(500 - text_array.length).fill(0)); // Filling with zeros instead of strings
-
-            const padded_int_array = Array(500).fill(0);
-            for (let i = 0; i < padded_text_array.length; i++) {
-                padded_int_array[i] = parseInt(padded_text_array[i]);
-                // error filling with NaN instead of int tokens
+            console.log(tokenizer[word]);
+            if (tokenizer[word]) {
+                return tokenizer[word];
+            } else {
+                return tokenizer['<UNKNOWN_WORD>'];
             }
-            console.log(padded_int_array);
-            const input = tensor2d(padded_int_array, [1, 500]); // Use tf.tensor2d for a 2D tensor
-            const prediction = model.predict(input);
-            // ! What is the prediction object ???
-            console.log(prediction.arraySync()[0][0]);
-            // https://medium.com/@andrew.w.sale/deploying-text-classification-from-keras-to-tensorflow-js-92c614dca4ec
-        };
+        } catch (error) {
+            // Handle any errors that occurred during fetching or importing
+            console.error('Error occurred while tokenizing:', error);
+            throw error;
+        }
+    };
 
-        const runPrediction = async () => {
-            const loadedModel = await loadModel();
-            predict(text, loadedModel);
-        };
+    const predict = async (text: string, model: any) => {
+        const text_array = text.split(' ');
+        const padded_text_array = text_array
+            .slice(0, 500)
+            .concat(Array(500 - text_array.length).fill('<UNKNOWN_WORD>'));
 
-        runPrediction();
-    }, [text]);
+        const padded_int_array = new Array(500).fill(0);
+        for (let i = 0; i < padded_text_array.length; i++) {
+            const token = padded_text_array[i];
+            try {
+                const tokenizer: any = await import(
+                    '../../assets/polarity/tokenizer.json'
+                );
+                if (tokenizer[token]) {
+                    padded_int_array[i] = tokenizer[token];
+                } else {
+                    padded_int_array[i] = tokenizer['<UNKNOWN_WORD>'];
+                }
+            } catch (error) {
+                console.error('Error occurred while tokenizing:', error);
+                throw error;
+            }
+        }
+
+        console.log(padded_int_array);
+        const input = tensor2d(padded_int_array, [1, 500]);
+        const prediction = model.predict(input);
+        console.log(prediction.arraySync()[0][0]);
+        setOutput(prediction.arraySync()[0][0].toString());
+        // https://medium.com/@andrew.w.sale/deploying-text-classification-from-keras-to-tensorflow-js-92c614dca4ec
+    };
+
+    const runPrediction = async () => {
+        const loadedModel = await loadModel();
+        predict(text, loadedModel).catch((err) => console.log(err));
+    };
 
     return (
         <Card
@@ -60,7 +86,8 @@ export const IAPrediction = ({ pathToModel }: FormCardProps) => {
                 value={text}
                 onChange={(e) => setText(e.target.value)}
             />
-            <H1>Output</H1>
+            <Button onClick={() => runPrediction()}>Predict</Button>
+            <H1>Output: {output}</H1>
         </Card>
     );
 };
