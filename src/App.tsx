2ac8e7d374d39ab191535bdb8ef274cc7a36e6c5
jeandtx <jean.doutriaux@gmail.com>
2023-06-19 13:35:37 +0200

    correction implementation home

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
 import { NavBar } from './components/navbar/navbar';
-import styles from './App.module.scss';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './screens/home/home';
 import { Plans } from './screens/plans/plans';
 import { About } from './screens/about/about';
 import { Login } from './screens/login/login';
-  
-
-
-
-
-
-
+import { Footer } from './components/footer/footer';
 
 function App() {
     return (
-        
-            <Router>
-                <NavBar />
-             
-                
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/plans" element={<Plans />} />
-                    <Route path="/about" element={<About />} />
-                    
-                    <Route path="/login" element={<Login />} />
-                </Routes>
-            </Router>
-        
+        <Router>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/plans" element={<Plans />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+            <Footer />
+        </Router>
     );
 }
 export default App;
