82503db958d360454d42bca44ed61b952e368aaa
jeandtx <jean.doutriaux@gmail.com>
2023-06-23 20:39:28 +0200

    Sentiments & labels for list of reviews from scrap

--- src/screens/scrapping/scrapping.tsx
+++ src/screens/scrapping/scrapping.tsx
@@ -1,6 +1,37 @@
 import React from 'react';
 import styles from './scrapping.module.scss';
 import axios from 'axios';
+import TagsInput from '../../components/tags-input/tags-input';
+
+async function getEmotion(data: any) {
+    const response = await fetch(
+        'https://api-inference.huggingface.co/models/SamLowe/roberta-base-go_emotions',
+        {
+            headers: {
+                Authorization: 'Bearer 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',
+            },
+            method: 'POST',
+            body: JSON.stringify(data),
+        }
+    );
+    const result = await response.json();
+    return result;
+}
+
+async function getLabels(data: any) {
+    const response = await fetch(
+        'https://api-inference.huggingface.co/models/facebook/bart-large-mnli',
+        {
+            headers: {
+                Authorization: 'Bearer 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',
+            },
+            method: 'POST',
+            body: JSON.stringify(data),
+        }
+    );
+    const result = await response.json();
+    return result;
+}
 
 const ExtractASIN = (text: string) => {
     let ASINreg = new RegExp(/(?:\/)([A-Z0-9]{10})(?:$|\/|\?)/);
@@ -13,8 +44,23 @@ const ExtractASIN = (text: string) => {
 };
 export const Scrapping: React.FC = () => {
     const [text, setText] = React.useState('');
+    const [output, setOutput] = React.useState([]);
+    const [labels, setLabels]: any = React.useState([
+        'good',
+        'bad',
+        'expensive',
+        'cheap',
+        'clean',
+        'dirty',
+    ]);
+    const [result, setResult] = React.useState<any>(null);
 
     const scrapp = async () => {
+        if (!ExtractASIN(text)) {
+            alert('This url is not valid');
+            setText('');
+            return;
+        }
         const params = {
             api_key: 'D2132ABBBBF04A878B19738F53749EED',
             amazon_domain: 'amazon.com',
@@ -30,29 +76,102 @@ export const Scrapping: React.FC = () => {
                 for (const element of response) {
                     out.push(element.body.toString());
                 }
-                setText(out.join('\n'));
+                setOutput(out);
+                console.log(out);
+                return out;
             })
             .catch((error: any) => {
                 console.log(error);
             });
     };
 
+    const analyze = async (out: any) => {
+        const result: any = {};
+
+        for (let i = 0; i < out.length; i++) {
+            try {
+                const emotionResult = await getEmotion(out[i]);
+                const labelResult = await getLabels({
+                    inputs: out[i],
+                    parameters: { candidate_labels: labels },
+                });
+
+                result[i] = {
+                    review: out[i],
+                    emotion: emotionResult,
+                    labels: labelResult,
+                };
+            } catch (error) {
+                console.log('Error occurred:', error);
+            }
+        }
+
+        const transformed_result = Object.values(result).map((item: any) => {
+            const review = item.review;
+            const firstSentiment = item.emotion[0][0].label;
+            const firstLabel = item.labels.labels[0];
+
+            return { review, firstSentiment, firstLabel };
+        });
+        setResult(transformed_result);
+        console.log('Result:', transformed_result);
+    };
+
     return (
-        <div className={styles.pro}>
-            <h1>Enter an Amazon product</h1>
-            <textarea
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-                className={styles.textInput}
-            />
-            <button
-                className={styles.button}
-                onClick={() => {
-                    scrapp();
-                }}
-            >
-                Search !
-            </button>
+        <div>
+            {text == 'result' ? (
+                <div className={styles.pro}>
+                    <h1>List of reviews</h1>
+                    <button
+                        className={styles.button}
+                        onClick={() => {
+                            analyze(output);
+                        }}
+                    >
+                        Analyze !
+                    </button>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>Index</th>
+                                <th>Review</th>
+                                <th>Emotion</th>
+                                <th>Labels</th>
+                            </tr>
+                            {result.map((element: any, index: number) => (
+                                <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{element.review}</td>
+                                    <td>
+                                        <p>{element.firstSentiment}</p>
+                                    </td>
+                                    <td>{element.firstLabel}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            ) : (
+                <div className={styles.pro}>
+                    <h1>Enter an Amazon product</h1>
+                    <textarea
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                        className={styles.textInput}
+                    />
+                    <h1>Labels</h1>
+                    <TagsInput myTags={labels} setMyTags={setLabels} />
+                    <button
+                        className={styles.button}
+                        onClick={() => {
+                            setText('result');
+                            scrapp();
+                        }}
+                    >
+                        Search !
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
